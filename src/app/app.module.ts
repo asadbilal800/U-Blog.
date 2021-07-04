8e57dc14ab72183d5b749f9d15fbf310c3fcfe69
asad bilal <asadbilal800@gmail.com>
2021-07-04 14:04:35 +0500

    signup and login functionality added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
-import {RoutingModule} from "./routing.module";
+import {RoutingModule} from "./routing-module";
 import {RouterModule} from "@angular/router";
 import { HeaderComponent } from './component/header/header.component';
+import { SignupComponent } from './component/signup/signup.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     RoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing-module.ts
+++ src/app/routing-module.ts
@@ -1,11 +1,13 @@
 import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./component/login/login.component";
 import {NgModule} from "@angular/core";
+import {SignupComponent} from "./component/signup/signup.component";
 
 
 const routes : Routes = [
   {path : '', redirectTo:'login',pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
+  {path : 'signup', component: SignupComponent}
 ]
 
 
