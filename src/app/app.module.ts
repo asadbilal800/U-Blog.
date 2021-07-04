5e52d72e810ffcfebe542df209ef5894b44a5705
asad bilal <asadbilal800@gmail.com>
2021-07-04 13:32:31 +0500

    Login page design ready.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
+import {RoutingModule} from "./routing.module";
+import {RouterModule} from "@angular/router";
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
